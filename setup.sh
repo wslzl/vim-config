7aca6018a7ee2f734d08e543d9ac3ac390b62ec8
lzl <lzl@gmail.com>
2022-06-26 20:24:20 +0800

    modify

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!\bin\bash
 
-wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim --directory=~/.vim/autoload
+wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim --directory=./.vim/autoload
 
-ln -s $(pwd)/.vimrc ~/
-ln -s $(pwd)/.vim ~/
+ln -s ./.vimrc ~/
+ln -s ./.vim ~/
